e9feb2b6891aed4d53683c26022752e8df546c7b
Ayman Omer <aymangebrilomer@gmail.com>
2021-01-12 20:38:24 -0500

    code long lab

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./compontes/Header";
+import PokeList from "./compontes/PokeList";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <PokeList />
+    </div>
+  );
+}
+
+export default App;
